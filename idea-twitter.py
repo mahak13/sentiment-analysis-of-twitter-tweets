9617d047e0827f1d36a1645337780eb5d022b319
mahak13 <37860453+mahak13@users.noreply.github.com>
2018-05-19 13:48:42 +0530

    Update idea-twitter.py

--- idea-twitter.py
+++ idea-twitter.py
@@ -1,1 +1,22 @@
+# General:
+import tweepy           # To consume Twitter's API
+import pandas as pd     # To handle data
+import numpy as np      # For number computing
+
+# For plotting and visualization:
+from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'vjw86vvE6Rm3cQihPucTRkN1l'
+CONSUMER_SECRET = '6NDHa2zLD6g4R0vqEfF65B5jdHxB83zhYV7qDVm2rH9WlKyusZ'
+
+# Access:
+ACCESS_TOKEN  = '996110379896864769-05PXtePousw0UQ6AC3QNP1ZVn2YSORD'
+ACCESS_SECRET = 'EEQxnqOyiFh1PFBjXUiB1jFwYXy74k7hMg0khw1LW3qOU'
+
 
